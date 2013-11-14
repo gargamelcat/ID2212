d69aa84e173e81b0528cd02de1a520829406f873
heya87 <joel.rolli@gmail.com>
2013-11-14 20:32:30 +0100

    Simple MainWindow to test WindowBuilder Pro

---
+++ RockScissorsPaper/src/presentation/MainWindow.java
@@ -0,0 +1,62 @@
+package presentation;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JMenuItem;
+
+public class MainWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnMain = new JMenu("Main");
+		menuBar.add(mnMain);
+		
+		JMenuItem mntmNewGame = new JMenuItem("New Game");
+		mnMain.add(mntmNewGame);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mnMain.add(mntmExit);
+		
+		JMenuBar menuBar_1 = new JMenuBar();
+		mnMain.add(menuBar_1);
+	}
+
+}
