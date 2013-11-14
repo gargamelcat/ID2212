abfb66c5ec301285f72961262b5b82cf28338137
heya87 <joel.rolli@gmail.com>
2013-11-14 19:26:48 +0100

    Created interface to data layer

---
+++ RockScissorsPaper/src/Data/IData.java
@@ -0,0 +1,33 @@
+package Data;
+
+
+/**
+ * This interface defines every data that needs to be saved during a game.
+ * @author Joel Rolli
+ * @version 1.0
+ */
+
+public interface IData {
+
+	
+	/**
+	 * This method will add a player to the existing game.
+	 * @param An object of the new player.
+	 * @return True if it worked well, false if not.
+	 */	
+	boolean addPlayer (Player newPlayer);
+	
+	/**
+	 * This method will  update a players information. That includes the last move.
+	 * @param Player with updated information.
+	 * @return DTOcustomer
+	 */	
+	void updatePlayer(Player existingPlayer);
+	
+	/**
+	 * This method will return an object of Game. That contains all information about an actual game.
+	 * @return The actual game.
+	 */	
+	Game getGame();
+
+}
