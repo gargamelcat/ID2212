a5cf334c16af7231c54f023f3a57797e693615dd
albert <albert@localhost>
2013-12-04 23:14:58 +0100

    preparing preparedstatement

--- MarketPlace_JDBC/src/Server/DataBaseConnection.java
+++ MarketPlace_JDBC/src/Server/DataBaseConnection.java
@@ -16,17 +16,29 @@ import java.util.Map;
 
 public class DataBaseConnection {
 
-    public static final String TABLE_NAME = "ACCOUNT";
-    private PreparedStatement createAccountStatement;
-    private PreparedStatement findAccountStatement;
-    private PreparedStatement deleteAccountStatement;
+    public static final String TABLE_USERS = "Users";
+    public static final String TABLE_ITEMS = "Items";
+    public static final String TABLE_WISHES = "Wishes";
+    public static final String TABLE_SALES = "Sales";
+
+    private PreparedStatement registerUser;
+    private PreparedStatement findUserStatement;
+    private PreparedStatement unregisterUserStatement;
+    
+    private PreparedStatement getSalesStatement;
+    private PreparedStatement setSaleStatement;
+    private PreparedStatement deleteSaleStatement;
+    
+    private PreparedStatement addWishStatement;
+    private PreparedStatement findWishStatement;
+    
     private String datasource;
 
     public DataBaseConnection(String datasource)
             throws RemoteException, ClassNotFoundException, SQLException {
         super();
         this.datasource = datasource;
-        //Connection connection = createDatasource();
+        Connection connection = createDatasource();
        // prepareStatements(connection);
     }
 
@@ -36,21 +48,45 @@ public class DataBaseConnection {
         int tableNameColumn = 3;
         DatabaseMetaData dbm = connection.getMetaData();
         for (ResultSet rs = dbm.getTables(null, null, null, null); rs.next();) {
-            if (rs.getString(tableNameColumn).equals("Users")) {
+            if (rs.getString(tableNameColumn).equals("Users") || rs.getString(tableNameColumn).equals("Items")
+            	|| rs.getString(tableNameColumn).equals("Wishes") || rs.getString(tableNameColumn).equals("Sales")) {
+            	System.out.println("Tables exists! Not being created :)");
                 exist = true;
                 rs.close();
                 break;
             }
         }
         if (!exist) {
+        	System.out.println("HELLO LETS CREATE TABLES!");
             Statement statement = connection.createStatement();
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS" + "`Users` ("
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS" + TABLE_USERS + "("
             		   + "`name` varchar(16) COLLATE latin1_general_ci NOT NULL,"
             		   + "`password` varchar(16) COLLATE latin1_general_ci NOT NULL,"
             		   + "`sales` int(11) NOT NULL,"
             		   + "`purchases` int(11) NOT NULL,"
             		  + "PRIMARY KEY (`name`)"
             		 + ")"  );
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS" + TABLE_ITEMS +  "("
+          		   + "`itemID` int(11) NOT NULL auto_increment,"
+         		   + "`name` varchar(16) COLLATE latin1_general_ci NOT NULL,"
+         		   + "`price` int(11) NOT NULL,"
+         		   + "`amount` int(11) NOT NULL,"
+         		  + "PRIMARY KEY (`itemID`)"
+         		 + ")"  );
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS" + TABLE_WISHES + "("
+          		   + "`fkw_name` varchar(16) COLLATE latin1_general_ci NOT NULL,"
+          		   + "`fkw_itemID` int(11) NOT NULL,"
+          		   + "FOREIGN KEY (fkw_name) REFERENCES Users(name),"
+          		   + "FOREIGN KEY (fkw_itemID) REFERENCES Items(itemID),"
+          		   + "primary key (fkw_name, fkw_itemID)"
+          		 + ")"  );
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS" + TABLE_SALES + "("
+           		   + "`fks_name` varchar(16) COLLATE latin1_general_ci NOT NULL,"
+           		   + "`fks_itemID` int(11) NOT NULL,"
+          		   + "FOREIGN KEY (fks_name) REFERENCES Users(name),"
+          		   + "FOREIGN KEY (fks_itemID) REFERENCES Items(itemID),"
+           		   + "primary key (fks_name, fks_itemID)"
+           		 + ")"  );
         }
         return connection;
     }
@@ -65,12 +101,26 @@ public class DataBaseConnection {
     }
 
     private void prepareStatements(Connection connection) throws SQLException {
-        createAccountStatement = connection.prepareStatement("INSERT INTO "
-                + TABLE_NAME + " VALUES (?, 0)");
-        findAccountStatement = connection.prepareStatement("SELECT * from "
-                + TABLE_NAME + " WHERE NAME = ?");
-        deleteAccountStatement = connection.prepareStatement("DELETE FROM "
-                + TABLE_NAME + " WHERE name = ?");
+        registerUser = connection.prepareStatement("INSERT INTO "
+                + TABLE_USERS + " VALUES (?, ?, 0, 0)");
+        findUserStatement = connection.prepareStatement("SELECT * from "
+                + TABLE_USERS + " WHERE NAME = ?");
+        unregisterUserStatement = connection.prepareStatement("DELETE FROM "
+                + TABLE_USERS + " WHERE name = ?");
+        
+        
+        getSalesStatement = connection.prepareStatement("TO BE DONE");
+        
+        setSaleStatement = connection.prepareStatement("INSERT INTO "
+                + TABLE_SALES + " VALUES (?, ?)");
+        
+        deleteSaleStatement = connection.prepareStatement("DELETE FROM "
+                + TABLE_SALES + " WHERE fks_name = ?");
+        
+        findWishStatement = connection.prepareStatement("BLA");
+        
+        findWishStatement = connection.prepareStatement("BLA");
+        
     }
 
     
